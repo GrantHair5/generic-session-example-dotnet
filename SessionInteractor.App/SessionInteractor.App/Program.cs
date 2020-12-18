9e4c8df971a5683ef5f10164bc915214a3b5370c
Grant Hair <grant.hair@arnoldclark.com>
2020-12-18 17:15:00 +0000

    Get build running

--- SessionInteractor.App/SessionInteractor.App/Program.cs
+++ SessionInteractor.App/SessionInteractor.App/Program.cs
@@ -9,37 +9,37 @@ namespace SessionInteractor.App
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello and welcome to the session interactor");
+            //Console.WriteLine("Hello and welcome to the session interactor");
 
-            var GrantId = Guid.NewGuid();
-            var DioId = Guid.NewGuid();
-            var car1Id = Guid.NewGuid();
+            //var GrantId = Guid.NewGuid();
+            //var DioId = Guid.NewGuid();
+            //var car1Id = Guid.NewGuid();
 
-            SessionStore.SetValue(new Person
-            {
-                Name = "Grant"
-            }, GrantId);
+            //SessionStore.SetValue(new Person
+            //{
+            //    Name = "Grant"
+            //}, GrantId);
 
-            var grant = SessionStore.GetValue<Person>(GrantId);
+            //var grant = SessionStore.GetValue<Person>(GrantId);
 
-            Console.WriteLine(grant.Name);
+            //Console.WriteLine(grant.Name);
 
-            SessionStore.SetValue(new Person
-            {
-                Name = "Dio"
-            }, DioId);
+            //SessionStore.SetValue(new Person
+            //{
+            //    Name = "Dio"
+            //}, DioId);
 
-            SessionStore.SetValue(new Vehicle
-            {
-                MakeModel = "Mazda Miata 1.4L",
-                Registration = "B1G B4LL5"
-            }, car1Id);
+            //SessionStore.SetValue(new Vehicle
+            //{
+            //    MakeModel = "Mazda Miata 1.4L",
+            //    Registration = "B1G B4LL5"
+            //}, car1Id);
 
-            var diosCar = SessionStore.GetValue<Vehicle>(car1Id);
+            //var diosCar = SessionStore.GetValue<Vehicle>(car1Id);
 
-            Console.WriteLine(JsonConvert.SerializeObject(diosCar, Formatting.Indented));
+            //Console.WriteLine(JsonConvert.SerializeObject(diosCar, Formatting.Indented));
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }
